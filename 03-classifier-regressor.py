e6fe5ac69c2da5e2e8d47350a315bc54be60a045
Fabrizio <fabri.fubels@gmail.com>
2020-06-15 20:55:34 +0200

    Updated 03-classifier-regressor

--- 03-classifier-regressor.py
+++ 03-classifier-regressor.py
@@ -15,8 +15,9 @@ By measure mean squared error of regressor!
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 
 # We have right and predicted targets
 y_true = np.array([  # Expected outputs
@@ -40,6 +41,28 @@ y_pred = np.array([  # Model results
 ])
 
 
-# We need to know the model accuracy
+# We need to know the model mean squared error
 mse = mean_squared_error(y_true, y_pred)
 print('MSE', mse)
+
+# R² = ratio between the "mean squared error" of an imaginary model
+#      and our mse
+
+# The imaginary model is a model which answer with a constant value foreach
+# dataset record. The constant is equals to the mean of desired answers.
+# The mean squared error of desired answers is called "baseline".
+
+# R² = 1 - (MSE/MSE_base) = [-∞, +1]
+r2 = r2_score(y_true, y_pred)
+print('r2', r2)
+
+residuals = y_pred - y_true
+
+plt.scatter(y_true, residuals)
+plt.xlabel('True')
+plt.ylabel('Error')
+
+plt.show()
+
+# By finding "patterns" inside the graph,
+# it's possible to find improvable data in features
