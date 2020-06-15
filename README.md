b4feb2496fa9b2657c08c3054406a5ff78632329
Fabrizio <fabri.fubels@gmail.com>
2020-06-15 18:51:56 +0200

    Hello World

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Studying Machine Learning by watching the following channel:
 [**Machine Learning in italiano col Pollo Watzlawick**](https://www.youtube.com/watch?v=hSZH6saoLBY&list=PLa-sizbCyh93c0nSPAb8k5ZZeOq4SBIl9)
 
 
+### Python Library
+
+In this repository will be used the Python Library ["scikit-learn"](scikit-learn).
---
+++ hello-world.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""
+1)
+"""
+
+from sklearn import datasets
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+# The input data is an iris flower dataset.
+# The desired output is the class of flower, by analyzing
+# the following parameters:
+#  - Sepal length
+#  - Sepal width
+#  - Petal length
+#  - Petal width
+
+iris_dataset = datasets.load_iris()
+
+X = iris_dataset.data       # Features
+y = iris_dataset.target     # Target
+
+print(iris_dataset['DESCR'])
+print()
+
+# Split data into training and test data
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# The test data is not used during learning, but is needed to measure
+# the final model learning quality
+
+# Execute learning
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+
+
+predicted_train = model.predict(X_train)
+predicted_test = model.predict(X_test)
+
+print('Train accuracy')
+print(accuracy_score(y_train, predicted_train))
+
+print('Test score')
+print(accuracy_score(y_test, predicted_test))
