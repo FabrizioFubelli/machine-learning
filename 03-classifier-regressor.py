1382fc9df99cc34aef256b262c442464a99bbabc
Fabrizio <fabri.fubels@gmail.com>
2020-06-15 20:35:16 +0200

    ++03-classifier-regressor.py

---
+++ 03-classifier-regressor.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+"""
+Machine Learning - Mean squared error, R-squared, Residual Plot
+
+Video:
+https://www.youtube.com/watch?v=CbESY3v80zg
+
+Classifiers:
+https://scikit-learn.org/stable/supervised_learning.html#supervised-learning
+
+
+How to chose the right algorithm?
+By measure accuracy of classifier!
+"""
+
+import numpy as np
+
+from sklearn.metrics import mean_squared_error
+
+# We have right and predicted targets
+y_true = np.array([  # Expected outputs
+    2,
+    1,
+    0,
+    3,
+    2,
+    -1,
+    5
+])
+
+y_pred = np.array([  # Model results
+    1,
+    1,
+    -1,
+    3,
+    0,
+    0,
+    4
+])
+
+
+# We need to know the model accuracy
+mse = mean_squared_error(y_true, y_pred)
+print('MSE', mse)
