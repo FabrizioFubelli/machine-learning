47a013d9bdc3305e1ebeae571e715f9fcfb232cc
Fabrizio Fubelli <fabrizio@fubelli.org>
2021-07-22 20:18:54 +0200

    ++Train a predictive model

--- 04-confusion-matrix.py
+++ 04-confusion-matrix.py
@@ -8,13 +8,14 @@ https://youtu.be/9G3l6c2EIfM
 
 
 How to measure the performance of a classifier?
-Accuracy and Confusion Matrix
+Thanks to Accuracy and Confusion Matrix!
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.metrics import accuracy_score
+from scikitplot.metrics import plot_confusion_matrix
 
 y_test = np.array([  # Expected outputs
     'basket',
@@ -38,3 +39,6 @@ y_pred = np.array([  # Model results
 
 accuracy = accuracy_score(y_test, y_pred)
 print('Accuracy', accuracy)
+
+plot_confusion_matrix(y_test, y_pred)
+plt.show()
---
+++ 05-train-predictive-model.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""
+Machine Learning - Train a predictive model
+
+Video:
+https://youtu.be/7YDWaTKtCdI
+
+LinearRegression:
+https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
+"""
+
+from sklearn.datasets import load_boston
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import train_test_split
+
+import numpy as np
+
+np.random.seed(2)
+
+dataset = load_boston()
+
+# X contains the features
+X = dataset['data']
+# y contains the target we want
+y = dataset['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+model = LinearRegression()
+model.fit(X_train, y_train)     # Train model from data
+
+p_train = model.predict(X_train)     # Predict X_train after training
+p_test = model.predict(X_test)       # Predict X_test after training
+
+mae_train = mean_absolute_error(y_train, p_train)
+mae_test = mean_absolute_error(y_test, p_test)
+print('MAE train', mae_train)
+print('MAE test', mae_test)
+
+# We need to know the model mean squared error
+mse_train = mean_squared_error(y_train, p_train)
+mse_test = mean_squared_error(y_test, p_test)
+print('MSE train', mse_train)
+print('MSE test', mse_test)
