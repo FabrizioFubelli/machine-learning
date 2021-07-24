b8e1eef6b32e9be0abba04d811a6fcd25c5d545e
Fabrizio Fubelli <fabrizio@fubelli.org>
2021-07-24 11:01:07 +0200

    ++lesson 11

--- 10-null-values.py
+++ 10-null-values.py
@@ -27,7 +27,9 @@ transformers = [
     ['age_imputer', SimpleImputer(), [0]],
     ['sex_imputer', SimpleImputer(strategy='constant', fill_value='n.d.'), [1]],
 ]
+
 ct = ColumnTransformer(transformers)
+
 X = ct.fit_transform(X)
 
 print(X)
